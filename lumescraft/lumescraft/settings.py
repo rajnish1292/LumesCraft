fe0f669e9285e75e315a606524a360214be1c6e2
rajnish-1292 <rajnish@bnsanalytics.com>
2021-11-20 15:39:07 +0530

    some changes

--- lumescraft/lumescraft/settings.py
+++ lumescraft/lumescraft/settings.py
@@ -51,6 +51,15 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        'rest_framework.permissions.AllowAny',
+    ]
+}
+
 ROOT_URLCONF = 'lumescraft.urls'
 
 TEMPLATES = [
